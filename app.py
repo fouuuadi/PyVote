814a85a7d36dd4907ebbcedd8750b1a5f4855897
Oscarj-jqt <jacquetoscar0@gmail.com>
2024-12-17 19:47:54 +0100

    màj des collections

--- app.py
+++ app.py
@@ -4,40 +4,46 @@
 #url_for : Génere les URL   
 #redirect : redirection HTTP
 import os
-from flask import Flask,render_template, jsonify, request, url_for, redirect
+from flask import Flask,render_template, jsonify, request, url_for, redirect, session
+from flask_session import Session
 #Object d'interraction avec MongoDB
 from pymongo import MongoClient
 #Gestion des ids de MongoDB
 from bson.objectid import ObjectId
 from bson import json_util
+from mongoDB.config.connection_db import get_database
+from mongoDB.config.initialize_db import initialize_collections
+from api.routes.auth import auth_bp
+from utils.decorators import login_required
+
+
 
 app = Flask(__name__)
 
-#Configuration de la base de donnee MongoDB
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-#Client MongoDB
-client = MongoClient(MONGO_URI)
-db = client["admin"]
-collection = db["restaurants"]
-
-@app.route('/', methods=['GET'])
-def index():
-    #tjrs render le fichier enfant Pas le parent 
-    return render_template('layout.html')
-
-@app.route('/restaurants', methods=['GET'])
-def get_restaurants():
-#     # Récupérer les 10 premiers restaurants, triés par nom
-#     cursor = collection.find().sort("name", 1).limit(10)
-#     # Convertir les données en JSON compatible avec Flask
-#     data = json_util.dumps(cursor)
-#     return data, 200
-    cursor = collection.find().sort("name", 1).limit(10)
-    restaurants = list(cursor)  # Convertir le curseur en liste
-    return render_template('layout.html', restaurants=restaurants)
+# Enregistrer les Blueprint
+# Avec le prefix sa sera /auth/signup et non /signup
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 
 
+app.secret_key = os.getenv("SECRET_KEY", "fallback_default_key")
+
+
+
+
+
+
+# initialisation des collections
+# crée les collections si elles sont inexistantes.
+initialize_collections()
+
+@app.route('/')
+@login_required
+def home():
+    return render_template('home.html', pseudo=session['pseudo'])
+
+#Le port sur lequel ton serveur flask fonctionne
+FLASK_PORT = os.getenv("FLASK_PORT")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4001)
+    app.run(debug=True, port=FLASK_PORT)
